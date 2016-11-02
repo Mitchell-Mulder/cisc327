b6b29b4c361727620971d11545916fd532d34d53
Brandon Bloch <brandon@bloch.ca>
2016-11-01 22:33:17 -0400

    Reversed output validation location to iterate over expected files

--- test/validate.sh
+++ test/validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(ls test/output/transactions)
+for i in $(ls test/expected/transactions)
 do
     echo "Checking outputs of test $i"
     diff test/output/transactions/$i test/expected/transactions/test$i
