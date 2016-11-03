99b1610151bb1bc8389256cefec234897f9aa85e
Brandon Bloch <brandon@bloch.ca>
2016-11-02 20:46:39 -0400

    Removed “un”-expected transaction logs, updated test to only diff ones that should exist

--- test/test.sh
+++ test/test.sh
@@ -26,5 +26,7 @@ for i in $(ls test/expected/transactions)
 do
     echo "Checking outputs of test $i" >> ${OUTFILE}
     diff test/output/transactions/$i test/expected/transactions/$i >> ${OUTFILE}
-    diff test/output/logs/$i.log test/expected/logs/$i >> ${OUTFILE}
+    if [ -f test/expected/logs/$i ]; then
+        diff test/output/logs/$i.log test/expected/logs/$i >> ${OUTFILE}
+    fi
 done
