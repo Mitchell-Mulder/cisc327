a07800df5eb3c7b837ce7eba873b27c77c0f25c1
Brandon Bloch <brandon@bloch.ca>
2016-12-01 13:50:58 -0500

    Use null again

--- test/integration/daily.sh
+++ test/integration/daily.sh
@@ -8,7 +8,7 @@
 for i in $(ls $1/input)
 do
     echo "Running test $i"
-    ./main.py $1/accounts.txt $1/transactions/$i < $1/input/$i
+    ./main.py $1/accounts.txt $1/transactions/$i < $1/input/$i > /dev/null
 done
 
 ./backend.py $1/master_accounts.txt $1/accounts.txt $1/transactions
